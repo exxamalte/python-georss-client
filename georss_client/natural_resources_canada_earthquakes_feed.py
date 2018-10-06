cb73ae8f4b89501a351de486d68d4e50a4d85e31
Malte Franken <exxamalte@users.noreply.github.com>
2018-10-06 23:11:24 +1000

    added minimum magnitude filter and remove category filter

--- georss_client/natural_resources_canada_earthquakes_feed.py
+++ georss_client/natural_resources_canada_earthquakes_feed.py
@@ -36,17 +36,23 @@ class NaturalResourcesCanadaEarthquakesFeed(GeoRssFeed):
     """Natural Resources Canada Earthquakes feed."""
 
     def __init__(self, home_coordinates, language, filter_radius=None,
-                 filter_categories=None):
+                 filter_minimum_magnitude=None):
         """Initialise this service."""
         if language in URLS:
             super().__init__(home_coordinates, URLS[language],
-                             filter_radius=filter_radius,
-                             filter_categories=filter_categories)
+                             filter_radius=filter_radius)
             self._language = language
+            self._filter_minimum_magnitude = filter_minimum_magnitude
         else:
             _LOGGER.error("Unknown feed language %s", language)
             raise GeoRssException("Feed language must be one of %s".format(
                 URLS.keys()))
+
+    def __repr__(self):
+        """Return string representation of this feed."""
+        return '<{}(home={}, url={}, radius={}, magnitude={})>'.format(
+            self.__class__.__name__, self._home_coordinates, self._url,
+            self._filter_radius, self._filter_minimum_magnitude)
 
     def _new_entry(self, home_coordinates, rss_entry, global_data):
         """Generate a new entry."""
@@ -56,10 +62,12 @@ class NaturalResourcesCanadaEarthquakesFeed(GeoRssFeed):
     def _filter_entries(self, entries):
         """Filter the provided entries."""
         entries = super()._filter_entries(entries)
-        if self._filter_categories:
+        if self._filter_minimum_magnitude:
+            # Return only entries that have an actual magnitude value, and
+            # the value is equal or above the defined threshold.
             return list(filter(lambda entry:
-                               entry.category in self._filter_categories,
-                               entries))
+                               entry.magnitude and entry.magnitude >= self.
+                               _filter_minimum_magnitude, entries))
         return entries
 
 
