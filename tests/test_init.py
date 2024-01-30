dda501c6ca262410148b538cc5ef2330d9233a55
Malte Franken <exxamalte@users.noreply.github.com>
2024-01-31 08:15:12 +1100

    Add Python 3.12 support (#45)
    * add python 3.12 support
    
    * compare less precisely
    
    * compare less precisely

--- tests/test_init.py
+++ tests/test_init.py
@@ -4,6 +4,7 @@ import unittest
 from unittest import mock
 from unittest.mock import MagicMock
 
+import pytest
 import requests
 
 from georss_client import (
@@ -109,7 +110,10 @@ class TestGeoRssFeed(unittest.TestCase):
 
         feed_entry = entries[0]
         assert feed_entry.external_id == "1234"
-        assert feed_entry.coordinates == (-34.93728111547821, 148.59710883878262)
+        assert feed_entry.coordinates == (
+            pytest.approx(-34.93728111547821),
+            pytest.approx(148.59710883878262),
+        )
         self.assertAlmostEqual(feed_entry.distance_to_home, 491.7, 1)
 
         feed_entry = entries[1]
@@ -119,7 +123,10 @@ class TestGeoRssFeed(unittest.TestCase):
 
         feed_entry = entries[2]
         assert feed_entry.external_id == "3456"
-        assert feed_entry.coordinates == (-29.962746645660683, 152.43090880416074)
+        assert feed_entry.coordinates == (
+            pytest.approx(-29.962746645660683),
+            pytest.approx(152.43090880416074),
+        )
         self.assertAlmostEqual(feed_entry.distance_to_home, 176.5, 1)
 
     @mock.patch("requests.Request")
