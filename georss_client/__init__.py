a882f6001b1f48c2ad2b1cda0a3628f39ee1be09
Malte Franken <exxamalte@users.noreply.github.com>
2024-01-31 20:03:39 +1100

    provide backwards compatibility

--- georss_client/__init__.py
+++ georss_client/__init__.py
@@ -1,5 +1,11 @@
 """Base class for GeoRSS services."""
 from georss_client.consts import ATTR_ATTRIBUTION  # noqa: F401
 
+from .consts import (  # noqa: F401
+    CUSTOM_ATTRIBUTE,
+    UPDATE_ERROR,
+    UPDATE_OK,
+    UPDATE_OK_NO_DATA,
+)
 from .feed import GeoRssFeed  # noqa: F401
 from .feed_entry import FeedEntry  # noqa: F401
