4e64569a59708dca3e32ccb13158a52e2a164a03
Malte Franken <exxamalte@users.noreply.github.com>
2024-01-31 18:31:08 +1100

    fix import

--- georss_client/__init__.py
+++ georss_client/__init__.py
@@ -1,5 +1,5 @@
 """Base class for GeoRSS services."""
 from georss_client.consts import ATTR_ATTRIBUTION  # noqa: F401
 
-from .feed import Feed  # noqa: F401
+from .feed import GeoRssFeed  # noqa: F401
 from .feed_entry import FeedEntry  # noqa: F401
